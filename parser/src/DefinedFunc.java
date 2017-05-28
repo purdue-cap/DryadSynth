44be2a0110c608a0507be2edc992c829c5d1cfe9
Kangjing Huang <huangkangjing@purdue.edu>
2017-05-27 22:56:35 -0400

    Implemented a rewrite method for DefinedFunc
    This is essentially the function symbol substitution method, the codes
    are refined so it could work as a method for DefinedFunc, which makes
    more sense

--- parser/src/DefinedFunc.java
+++ parser/src/DefinedFunc.java
@@ -29,6 +29,60 @@ public class DefinedFunc {
         return definition.substitute(args, argList);
     }
 
+    public Expr rewrite(Expr orig, FuncDecl func) {
+        Stack<Expr> todo = new Stack<Expr>();
+        todo.push(orig);
+        Map<Expr, Expr> cache = new HashMap<Expr, Expr>();
+
+        boolean visited;
+        Expr expr, newExpr, body;
+        Expr [] args, newArgsArray, bodyArray;
+        List<Expr> newArgs = new ArrayList<Expr>();
+        FuncDecl exprFunc;
+        while (!todo.empty()) {
+            expr = todo.peek();
+            if (expr.isVar()) {
+                todo.pop();
+                cache.put(expr, expr);
+            } else if (expr.isApp()) {
+                visited = true;
+                newArgs.clear();
+                args = expr.getArgs();
+                for (Expr arg: args) {
+                    if (!cache.containsKey(arg)) {
+                        todo.push(arg);
+                        visited = false;
+                    } else {
+                        newArgs.add(cache.get(arg));
+                    }
+                }
+                if (visited) {
+                    todo.pop();
+                    exprFunc = expr.getFuncDecl();
+                    newArgsArray = newArgs.toArray(new Expr[newArgs.size()]);
+                    if (exprFunc.equals(func)) {
+                        newExpr = this.apply(newArgsArray);
+                    } else {
+                        newExpr = expr.update(newArgsArray);
+                    }
+                    cache.put(expr, newExpr);
+                }
+            } else {
+                assert expr.isQuantifier();
+                body = ((Quantifier)expr).getBody();
+                if (cache.containsKey(body)) {
+                    todo.pop();
+                    bodyArray = new Expr[]{ cache.get(body) };
+                    newExpr = expr.update(bodyArray);
+                    cache.put(expr, newExpr);
+                } else {
+                    todo.push(body);
+                }
+            }
+        }
+        return cache.get(orig);
+    }
+
     public String getName() {
         return name;
     }
