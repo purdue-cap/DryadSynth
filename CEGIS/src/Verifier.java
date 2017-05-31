c2ef22b45bcc69452bba84dd3b74c8ec9a961b73
Yanjun Wang <wang3204@purdue.edu>
2017-05-30 20:58:07 -0400

    Use spec extracted from parser instead of hardcode

--- CEGIS/src/Verifier.java
+++ CEGIS/src/Verifier.java
@@ -8,16 +8,20 @@ public class Verifier {
 	private int numVar;
 	private int numFunc;
 	private IntExpr[] var;
+	private BoolExpr finalConstraint;
+	private SygusExtractor extractor;
 
-	public Verifier(Context ctx, int numVar, int numFunc, IntExpr[] var) {
+	public Verifier(Context ctx, int numVar, int numFunc, IntExpr[] var, SygusExtractor extractor) {
 		this.numVar = numVar;
 		this.numFunc = numFunc;
 		this.ctx = ctx;
 		this.s = ctx.mkSolver();
 		this.var = var;
+		this.finalConstraint = extractor.finalConstraint;;
+		this.extractor = extractor;
 	}
 
-	public BoolExpr max2Prop(ArithExpr[] functions) {
+	/*public BoolExpr max2Prop(ArithExpr[] functions) {
 		//BoolExpr max2Prop = ctx.mkOr(ctx.mkAnd(ctx.mkGe(var[0], var[1]), ctx.mkEq(ctx.mkApp(eval, var[0], var[1], ctx.mkInt(0), ctx.mkInt(0)), var[0]))
 		//	, ctx.mkAnd(ctx.mkLt(var[0], var[1]), ctx.mkEq(ctx.mkApp(eval, var[0], var[1], ctx.mkInt(0), ctx.mkInt(0)), var[1])));
 
@@ -35,21 +39,40 @@ public class Verifier {
 			, ctx.mkOr(ctx.mkEq(functions[0], var[0]), ctx.mkEq(functions[0], var[1]), ctx.mkEq(functions[0], var[2])));
 
 		return max3Prop;
-	}
+	}*/
 
 	public Status verify(ArithExpr[] functions) {
 
-		BoolExpr spec = max2Prop(functions);
+		Expr spec = finalConstraint;
+
+		int i = 0;
+		for (FuncDecl f : extractor.requests.values()) {
+			DefinedFunc df = new DefinedFunc(ctx, var, functions[i]);
+			spec = df.rewrite(spec, f);
+			i = i + 1;
+		}
+
+		int j = 0;
+		for(Expr expr: extractor.vars.values()) {
+			spec = 	spec.substitute(expr, var[j]);
+			j = j + 1;
+		}
+
+		//System.out.println("Specification : ");
+		//System.out.println(ctx.mkNot((BoolExpr)spec));
+
+		//BoolExpr spec = max2Prop(functions);
 		//BoolExpr spec = max3Prop(functions);
 
 		s.push();
-		s.add(ctx.mkNot(spec));
+		s.add(ctx.mkNot((BoolExpr)spec));
 		//System.out.println("Verifying... Formula: ");
 		//System.out.println(s);
 
 		Status status = s.check();
 		s.pop();
 		return status;
+		//return Status.UNSATISFIABLE;
 
 	}
 
