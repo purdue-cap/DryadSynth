52230eaead12c32bfeeae716f54c1dfa115be4a4
Kangjing Huang <huangkangjing@purdue.edu>
2017-06-05 19:20:04 -0400

    Clean up some codes

--- src/CEGIS/Synth.java
+++ src/CEGIS/Synth.java
@@ -17,7 +17,6 @@ public class Synth {
 		HashMap<String, String> cfg = new HashMap<String, String>();
 		cfg.put("model", "true");
 		Context ctx = new Context(cfg);
-		//Context z3ctx = new Context();
 
 		ANTLRErrorStrategy es = new CustomErrorStrategy();
 		parser.setErrorHandler(es);
@@ -43,33 +42,6 @@ public class Synth {
 
 		long estimatedTime = System.currentTimeMillis() - startTime;
 		System.out.println("Runtime: " + estimatedTime);
-
-		/*System.out.println("Synth requests:");
-		for(FuncDecl func : extractor.requests.values()) {
-			System.out.println("Name:" + func.getName());
-			for(Sort sort : func.getDomain()) {
-				System.out.println("Has argument with type " + sort.getName());
-			}
-			System.out.println("Return type is " + func.getRange().getName());
-		}
-
-		System.out.println("Defined variables:");
-		for(Expr expr: extractor.vars.values()) {
-			System.out.println("Name:" + expr.toString() + " Type:" + expr.getSort().toString());
-		}
-
-		System.out.println("Defined functions:");
-		for(DefinedFunc func: extractor.funcs.values()) {
-			System.out.println("Name:" + func.getName() + " Definition:" + func.toString());
-		}
-
-		System.out.println("Constraints:");
-		for (Expr expr: extractor.constraints) {
-			System.out.println(expr);
-		}
-
-		System.out.println("Final Constraints:");
-		System.out.println(extractor.finalConstraint);*/
 
 	}
 }
