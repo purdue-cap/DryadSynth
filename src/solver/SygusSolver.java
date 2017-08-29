d54d1dc6868c2f4302588af6186f916b273327f2
Kangjing Huang <huangkangjing@purdue.edu>
2017-08-29 17:27:25 -0400

    Added solver for choosing algorithm and reorged

---
+++ src/solver/SygusSolver.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import com.microsoft.z3.*;
+
+public class SygusSolver {
+    Context z3ctx;
+    SygusExtractor extractor;
+
+    SygusSolver(Context z3ctx, SygusExtractor extractor) {
+        this.z3ctx = z3ctx;
+        this.extractor = extractor;
+    }
+
+    public void prescreen() {
+
+    }
+
+    public Thread[] initAlgoithm () {
+        
+    }
+
+    HashSet<String> validateCandidates() {
+
+    }
+
+
+}
