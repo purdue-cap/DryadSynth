249e1ad66973cd4c7ff96cf774a12dcaf17091c2
Peiyuan Shen <shen290@purdue.edu>
2017-12-04 12:48:55 -0500

    Delete Run.java

--- Run.java
+++
@@ -1,7 +0,0 @@
-public class Run{
-
-    public static void main(String[] args) throws Exception{
-    	SelectionBoard selectionBoard = new SelectionBoard();
-//    	selectionBoard.gameManager.run();  
-    }
-}
