2ad604a4e31cb86a96d1da69f575def829f7839a
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2018-10-05 21:19:36 -0400

    Modify exec.sh. tuning for webportal

--- exec.sh
+++ exec.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-BASE=`dirname $0`
-java -cp $BASE/classes/:$BASE/lib/antlr.jar:$BASE/lib/com.microsoft.z3.jar:$BASE/lib/jopt-simple.jar Run -t 1 -v "$@" 2>&1
+BASEDIR=$(dirname "$0")
+export CLASSPATH=$BASEDIR/classes/:$BASEDIR/lib/com.microsoft.z3.jar:$BASEDIR/lib/jopt-simple.jar
+java Run -t 1 -v $@
