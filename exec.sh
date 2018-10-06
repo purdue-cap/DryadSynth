a794d20a3da2ace44ec9b3287f3e80ba11738cc6
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2018-10-05 21:22:46 -0400

    Add a missing jar

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
-export CLASSPATH=$BASEDIR/classes/:$BASEDIR/lib/com.microsoft.z3.jar:$BASEDIR/lib/jopt-simple.jar
+export CLASSPATH=$BASEDIR/classes/:$BASEDIR/lib/com.microsoft.z3.jar:$BASEDIR/lib/jopt-simple.jar:$BASEDIR/lib/antlr.jar
 java Run -t 1 -v $@
